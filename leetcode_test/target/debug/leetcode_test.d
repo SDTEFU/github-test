66cfe83eb112d75f6e4f60de532809fc2f00f481
UTGEA <UGTRE@aa.com>
2023-04-21 18:28:49 +0800

    all in

--- leetcode_test/target/debug/leetcode_test.d
+++ leetcode_test/target/debug/leetcode_test.d
@@ -1,1 +1,1 @@
-/home/adgert/Desktop/test/my-git-test/leetcode_test/target/debug/leetcode_test: /home/adgert/Desktop/test/my-git-test/leetcode_test/src/main.rs
+/home/adgert/Desktop/test/github-test/leetcode_test/target/debug/leetcode_test: /home/adgert/Desktop/test/github-test/leetcode_test/src/main.rs
