401d576bd2a327378f05f4cf7aa2fdd408b9ffb4
UTGEA <UGTRE@aa.com>
2023-04-21 18:39:02 +0800

    all update

--- leetcode_test/target/debug/deps/leetcode_test-619749713a965975.d
+++ leetcode_test/target/debug/deps/leetcode_test-619749713a965975.d
@@ -1,5 +1,5 @@
-/home/adgert/Desktop/test/my-git-test/leetcode_test/target/debug/deps/leetcode_test-619749713a965975.rmeta: src/main.rs
+/home/adgert/Desktop/test/github-test/leetcode_test/target/debug/deps/leetcode_test-619749713a965975.rmeta: src/main.rs
 
-/home/adgert/Desktop/test/my-git-test/leetcode_test/target/debug/deps/leetcode_test-619749713a965975.d: src/main.rs
+/home/adgert/Desktop/test/github-test/leetcode_test/target/debug/deps/leetcode_test-619749713a965975.d: src/main.rs
 
 src/main.rs:
